ced32a43c23dbc204553ed268cc608418a04cad7
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-19 08:59:42 +0300

    added lodash

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ console.log(add(10, 10));
 
 // pradeti organizuoti savo koda su NPM
 // npm init
+// npm install --save lodash
+
+// node_modules direktorija yra visos musu projekto priklausomybes
+// sitas folderis neturetu buti pridedamas i git
+// .gitignore - faile nurodom failus is direktorijas kuriu nenorim prideti i git
