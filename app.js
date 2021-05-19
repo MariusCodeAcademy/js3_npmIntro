3b6aa2bcf73c495d218b06e4bdcbaabbe70559f6
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-19 10:44:49 +0300

    install nodeMon as dev dep

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const _ = require("lodash");
 
 // console.log("app.js");
 
-// console.log(add(10, 10));
+console.log(add(100, 50));
 
 // pradeti organizuoti savo koda su NPM
 // npm init
@@ -12,3 +12,13 @@ const _ = require("lodash");
 // node_modules direktorija yra visos musu projekto priklausomybes
 // sitas folderis neturetu buti pridedamas i git
 // .gitignore - faile nurodom failus is direktorijas kuriu nenorim prideti i git
+
+// idiegiti nodemon kaip dev dependancy - funkciionalumas naudojamas tik
+// aplikacijos kurimo metu
+// instaliuojam su --save-dev velevele
+// npm install nodemon --save-dev
+
+// sustabdyti procesa vykstanti consolje naudojam ctrl + c
+
+// instaliujam paketus globaliai - tai reiskia veiks visur
+//npm install -g nodemon
